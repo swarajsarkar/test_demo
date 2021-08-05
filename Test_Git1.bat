bb4a7cf7ee0076f8cf7fedb2e2ba5468d4150e8a
swarajsarkar <swaraj.sarkar@gmail.com>
2021-08-05 18:36:05 +0530

    Testing

--- Test_Git1.bat
+++ Test_Git1.bat
@@ -1,1 +1,24 @@
-echo "Hello World!!!"
+pipeline {
+    agent any
+
+    stages {
+        stage('Set up') {
+            steps {
+                echo 'Hello World'
+            }
+       } 
+       
+       stage('Building') {
+            steps {
+                echo 'Building Phase'
+            }
+       } 
+    
+       stage('Testing') {
+            steps {
+                echo 'Testing Phase'
+            }
+       }
+    }
+
+} 
